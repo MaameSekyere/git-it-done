3ec49538a11811c3d91265a6a810a72b80996c98
MaameSekyere <maameant@gmail.com>
2022-04-01 23:54:19 -0400

    Update js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,36 @@
-var getUserRepos = function (response) {
-  fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/octocat/repos" + user + "/repos";
+
+  //make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
-getUserRepos();
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+
+  console.log(event);
+};
+
+var displayRepos = function(repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
