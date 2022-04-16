9c0634c2664146fd2c568eb4a652e891b5a1c7c7
MaameSekyere <maameant@gmail.com>
2022-04-16 03:10:17 -0400

    added display warning function

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
+var repoNameEl = document.querySelector("#repo-name");
 issueContainerEl = document.querySelector("#issues-container");
+limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,13 +10,17 @@ var getRepoIssues = function (repo) {
       //response was successful
       response.json().then(function (data) {
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
     }
   });
 };
-getRepoIssues("maamesekyere/git-it-done");
 
 var displayIssues = function (issues) {
   if (issues.length === 0) {
@@ -51,3 +57,18 @@ var displayIssues = function (issues) {
     issueContainerEl.appendChild(issueEl);
   }
 };
+
+var displayWarning = function (repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
